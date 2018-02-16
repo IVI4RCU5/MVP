04de7298a86322851edc460f62eba64dff0ecca5
Marcus Hausler <marcus.hausler@gmail.com>
2018-02-16 16:50:09 -0500

    added some boilerplate code

---
+++ database/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS mvp;
+
+CREATE DATABASE mvp;
+
+USE mvp;
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE messages (
+  id INT NOT NULL AUTO_INCREMENT,
+  author INT,
+  content VARCHAR(300),
+  FOREIGN KEY author REFERENCES users(id)
+);
+
+CREATE TABLE friends (
+  user INT,
+  friend INT,
+  FOREIGN KEY user REFERENCES users(id),
+  FOREIGN KEY friend REFERENCES users(id)
+)
